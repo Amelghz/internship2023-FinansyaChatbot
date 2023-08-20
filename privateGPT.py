c77abc7005085d8cc73509dd4d999025fe695402
Amel Ghozzi <92169711+Amelghz@users.noreply.github.com>
2023-08-20 17:38:21 +0100

    Update privateGPT.py

--- privateGPT.py
+++ privateGPT.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# Import necessary modules
 from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings
@@ -9,8 +10,10 @@ import os
 import argparse
 import time
 
+# Load environment variables from .env file
 load_dotenv()
 
+# Load environment variables
 embeddings_model_name = os.environ.get("EMBEDDINGS_MODEL_NAME")
 persist_directory = os.environ.get('PERSIST_DIRECTORY')
 
@@ -20,17 +23,20 @@ model_n_ctx = os.environ.get('MODEL_N_CTX')
 model_n_batch = int(os.environ.get('MODEL_N_BATCH',8))
 target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
+# Import constants
 from constants import CHROMA_SETTINGS
-
+# Main function
 def main(tested_input):
     # Parse the command line arguments
     args = parse_arguments()
+    # Initialize HuggingFaceEmbeddings
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+    # Initialize Chroma vector store for retrieval
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
-    # Prepare the LLM
+    # Prepare the LLM based on model_type
     match model_type:
         case "LlamaCpp":
             llm = LlamaCpp(model_path=model_path, n_ctx=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
@@ -39,12 +45,12 @@ def main(tested_input):
         case _default:
             # raise exception if model_type is not supported
             raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
-        
+    # Initialize RetrievalQA based on the LLM and retriever    
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
     
     # Interactive questions and answers
     while True:
-        query =tested_input  #input("\nEnter a query: ")
+        query =tested_input  # Get input query from user
         if query == "exit":
             break
         if query.strip() == "":
@@ -56,17 +62,11 @@ def main(tested_input):
         answer, docs = res['result'], [] if args.hide_source else res['source_documents']
         end = time.time()
 
-        # Print the result
-        #print("\n\n> Question:")
-        #print(query)
-        #print(f"\n> Answer (took {round(end - start, 2)} s.):")
-        #print(answer)
         return answer
-        # Print the relevant sources used for the answer
-        #for document in docs:
-            #print("\n> " + document.metadata["source"] + ":")
-            #print(document.page_content)
+        
 
+
+# Parse command line arguments
 def parse_arguments():
     parser = argparse.ArgumentParser(description='privateGPT: Ask questions to your documents without an internet connection, '
                                                  'using the power of LLMs.')
@@ -80,5 +80,6 @@ def parse_arguments():
     return parser.parse_args()
 
 
+# Execute the main function when the script is run directly
 if __name__ == "__main__":
     main()
